56d110c5a55abb1f827796721a3106633d558963
Fabricio <fabricio.daza@gmail.com>
2020-06-07 19:51:37 -0400

    apiserver, first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require('vendor/autoload.php');
+//require_once('conn/dbconn.php');
+
+$app = new \Slim\App;
+
+/**/
+//http://localhost/srvr_jwd/datum/apiserver/hello/fabricio
+//https://v2.hurricanefabric.com/webutils/apiserver/hello/fabricio
+//https://www.hurricanefabric.com/webutils/apiserver/hello/fabricio
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+
+/**/
+
+//require_once('endpoints.php');
+
+
+$app->run();
